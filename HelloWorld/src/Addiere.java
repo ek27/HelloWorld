b168bafdb2bbec72d910e5e4390ff547a94cbb96
Karakurt.Esme <Karakurt.Esme@SK-448A5BBA21EA.2631.skool.net>
2016-09-26 15:29:19 +0200

    Addiere

---
+++ HelloWorld/src/Addiere.java
@@ -0,0 +1,20 @@
+
+public class Addiere {
+	
+	static int number = 0;
+
+	public static void main(String[] args) {
+/*		primAusgabe();  */
+		System.out.println(multipliziere(25,25));
+	
+	}
+
+
+	
+	
+	public static int multipliziere (int zahl1, int zahl2){
+		return zahl1*zahl2;
+
+
+} 
+}
