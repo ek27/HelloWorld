875f77321083d5a11f2a358a39b1f01cf708792c
Karakurt.Esme <Karakurt.Esme@SK-448A5BBA21EA.2631.skool.net>
2016-09-26 14:59:15 +0200

    Primzahlen (Probedivision)

--- HelloWorld/src/HelloWorld1.java
+++ HelloWorld/src/HelloWorld1.java
@@ -1,10 +1,15 @@
 
 public class HelloWorld1 {
 
+		static int zahl1 = 17;
+		static int zahl2 = 8;
+		
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World xcxc:-)");
+		System.out.println("Hello World :-)");
+		System.out.println(zahl1/zahl2);
 		
 	}
 
